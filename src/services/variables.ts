a7695ce593012ab03bc1b2b0902f5697366b86b0
karanchheti@gmail.com <Anuragchhetri@users.noreply.github.com>
2023-03-06 16:29:38 +0530

    frontend

--- src/services/variables.ts
+++ src/services/variables.ts
@@ -1,2 +1,2 @@
-export const API='https://mern-contact-backend-nzqqfppc6-anuragchhetri0302.vercel.app/contact'
+export const API='https://merncontact-backend.vercel.app/contact'
 // export const API='http://localhost:5001/contact/'
